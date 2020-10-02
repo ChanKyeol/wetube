d6ba9c7621ebb613f6fa33af1c12b4754d9298d0
coldflow <dev.coldflow@gmail.com>
2020-10-03 02:27:58 +0900

    #3 complite

--- db.js
+++ db.js
@@ -1,54 +1,72 @@
-export const videos = [
-  {
-    id: 332231,
-    title: "Vodeo awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 12122,
-      name: "Ck",
-      email: "c@k.com",
-    },
-  },
-  {
-    id: 3322211133,
-    title: "Vodeo super",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 12122,
-      name: "Ck",
-      email: "c@k.com",
-    },
-  },
-  {
-    id: 55446,
-    title: "Vodeo good",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 12122,
-      name: "Ck",
-      email: "c@k.com",
-    },
-  },
-  {
-    id: 999977,
-    title: "Vodeo yaps",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 12122,
-      name: "Ck",
-      email: "c@k.com",
-    },
-  },
-];
+// export const videos = [
+//   {
+//     id: 332231,
+//     title: "Vodeo awesome",
+//     description: "This is something I love",
+//     views: 24,
+//     videoFile:
+//       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+//     creators: {
+//       id: 12122,
+//       name: "Ck",
+//       email: "c@k.com",
+//     },
+//   },
+//   {
+//     id: 3322211133,
+//     title: "Vodeo super",
+//     description: "This is something I love",
+//     views: 24,
+//     videoFile:
+//       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+//     creators: {
+//       id: 12122,
+//       name: "Ck",
+//       email: "c@k.com",
+//     },
+//   },
+//   {
+//     id: 55446,
+//     title: "Vodeo good",
+//     description: "This is something I love",
+//     views: 24,
+//     videoFile:
+//       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+//     creators: {
+//       id: 12122,
+//       name: "Ck",
+//       email: "c@k.com",
+//     },
+//   },
+//   {
+//     id: 999977,
+//     title: "Vodeo yaps",
+//     description: "This is something I love",
+//     views: 24,
+//     videoFile:
+//       "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+//     creators: {
+//       id: 12122,
+//       name: "Ck",
+//       email: "c@k.com",
+//     },
+//   },
+// ];
+
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  userNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅Connected to DB");
+const handleError = (error) =>
+  console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,8 @@
+import multer from "multer";
 import routes from "./routes";
+
+const multerVideo = multer({ dest: "uploads/videos/" });
+
 // 전역 범위에 변수를 추가하는 방법.
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
@@ -9,3 +13,5 @@ export const localsMiddleware = (req, res, next) => {
   };
   next();
 };
+
+export const uploadVideo = multerVideo.single("videoFile");
